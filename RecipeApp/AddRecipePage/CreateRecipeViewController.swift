00513ffdd69e4758ff8bca557d1e468bdeaa8fe0
DmitryLorents <zmey.task@gmail.com>
2023-03-15 22:15:28 +0600

    - add CreateRecipeViewController - add UITextField extension

---
+++ RecipeApp/AddRecipePage/CreateRecipeViewController.swift
@@ -0,0 +1,83 @@
+
+
+import UIKit
+import SnapKit
+
+class CreateRecipeViewController: UIViewController {
+    
+    var createRecipeLabel: UILabel!
+    var productImageView: UIImageView!
+    var nameTF: UITextField!
+    var servesView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        setLayout()
+
+    }
+    
+    func setup() {
+        createRecipeLabel = UILabel()
+        createRecipeLabel.translatesAutoresizingMaskIntoConstraints = false
+        createRecipeLabel.text = "Create recipe"
+        createRecipeLabel.font = UIFont.poppins(24, weight: PoppinsWeight.bold)
+        view.addSubview(createRecipeLabel)
+        
+        productImageView = UIImageView()
+        productImageView.translatesAutoresizingMaskIntoConstraints = false
+        productImageView.backgroundColor = .systemGray6
+        productImageView.layer.cornerRadius = 12
+        view.addSubview(productImageView)
+        
+        nameTF = UITextField()
+        nameTF.translatesAutoresizingMaskIntoConstraints = false
+        nameTF.layer.borderWidth = 1
+        nameTF.layer.cornerRadius = 10
+        nameTF.layer.borderColor = UIColor.primary50?.cgColor
+        nameTF.font = UIFont.poppins(14, weight: PoppinsWeight.regular)
+        nameTF.textColor = UIColor.neutral100
+        nameTF.indent(size: 15)
+        view.addSubview(nameTF)
+        
+        servesView = UIView()
+        servesView.translatesAutoresizingMaskIntoConstraints = false
+        servesView.backgroundColor = UIColor.neutral0
+        servesView.layer.cornerRadius = 12
+        view.addSubview(servesView)
+        
+    }
+    
+    func setLayout() {
+        
+        createRecipeLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(96)
+            make.leading.equalToSuperview().inset(19)
+            make.centerX.equalToSuperview()
+        }
+        
+        productImageView.snp.makeConstraints { make in
+            make.top.equalTo(createRecipeLabel).inset(44)
+            make.centerX.equalToSuperview()
+            make.leading.equalToSuperview().inset(16)
+            make.height.equalTo(200)
+        }
+        
+        nameTF.snp.makeConstraints { make in
+            make.top.equalTo(productImageView.snp.bottom).inset(-16)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(productImageView.snp.leading)
+            make.height.equalTo(44)
+        }
+        
+        servesView.snp.makeConstraints { make in
+            make.top.equalTo(nameTF.snp.bottom).inset(-15.6)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(productImageView.snp.leading)
+            make.height.equalTo(60)
+        }
+        
+    }
+    
+}
